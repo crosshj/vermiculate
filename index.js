027c629f5e11bce104cb92e1d129515eeee5fa83
harrison.cross <harrison.cross@transport4.com>
2021-03-10 13:42:36 -0500

    should be working in fiug

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//show-preview
 
 /* 
 
@@ -5,23 +6,29 @@
 
 */
 
-var main = require('./lib/main');
+import main from './lib/main.js';
+
+var style = document.createElement('style');
+style.innerHTML = `
+body {
+		display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: calc(100vh - 0em);
+	overflow: hidden;
+}
+canvas {
+		/*background: #00000040;*/
+	width: 100%;
+	/*max-width: 1024px;*/
+	ximage-rendering: pixelated;
+	ximage-rendering: crisp-edges;
+}
+`;
+
+document.body.append(style);
+//const CURRENT_CONFIG = 5; //some problems with this * pickbank
+const CURRENT_CONFIG = 12;
+window.process = { argv: [,,CURRENT_CONFIG] };
 main();
-
-// var constants = require('./lib/constants');
-// var pickbank = require('./lib/pickbank');
-
-// var sampleStrings = constants.sampleStrings;
-// var global = require('./lib/global');
-// var threads = global.getThreads();
-
-
-// global.maininit();
-
-
-// //console.log(sampleStrings[0].str);
-
-// // TODO:
-// pickbank();
-
-// //console.log(global.getBank()[0]);
