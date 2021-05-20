33c5915264ce4722465b89dcfb167c28b20d871e
crosshj <github@chimpjuice.com>
2021-05-20 10:12:03 -0400

    work from index.html vs index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-//show-preview
-
 /* 
-
 	http://www.eclipse.net/~gmcsf/fdm/
-	
+
 	THREAD TYPES:
 	0) not active?
 	1) straight w/ slight wiggle
@@ -13,16 +10,12 @@
 	5) wiggle | sinusoidal
 	6) oblong | stone-shaped
 	7) rounded 4-point star | clover
-	
+
 	> prey behavior
 	> selfbounce
-	
 */
 
 import main from './src/main.js';
-
-document.body.style.overflow = "hidden";
-document.head.innerHTML += `<link rel="stylesheet" href="index.css">`
 
 //const CURRENT_CONFIG = 5; //some problems with this * pickbank
 const CURRENT_CONFIG = 3;
